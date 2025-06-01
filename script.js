2413f66c119093e2c84c30da1cad6fd3f842f508
Pongsatorn <pongsatornrd@gmail.com>
2025-06-01 23:04:07 +0700

    Task Tracker Project

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log('Hello World!!')
+const form = document.getElementById('task-form');
+const input = document.getElementById('task-input');
+const taskList = document.getElementById('task-list');
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  const task = input.value.trim();
+  if (task) {
+    addTask(task);
+    input.value = '';
+  }
+});
+
+function addTask(taskText) {
+  const li = document.createElement('li');
+  li.innerHTML = `
+    <span>${taskText}</span>
+    <button onclick="removeTask(this)">Delete</button>
+  `;
+  taskList.appendChild(li);
+}
+
+function removeTask(button) {
+  button.parentElement.remove();
+}
